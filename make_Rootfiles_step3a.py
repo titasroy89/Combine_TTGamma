10fb274d03c7acf426686857f45739ac63b6a434
titasroy89 <titasroy89@gmail.com>
2017-12-13 16:37:03 -0600

    adding some datacards and python scripts

--- make_Rootfiles_step3a.py
+++ make_Rootfiles_step3a.py
@@ -4,14 +4,17 @@ import os
 random=TRandom3()
 
 _file  = TFile("/uscms_data/d3/troy2012/CMSSW_8_0_26_patch1/src/TTGammaSemiLep_13TeV/Plotting/histograms/mu/hists.root")
-file0 = TFile("Step1a_onlyMC.root","recreate")
+file0 = TFile("Step1_onlyMC.root","recreate")
 file0.mkdir("M3")
 file0.mkdir("ChHadIso")
 
 
 
-channels={"M3":["M3",5],
-          "noCut_ChIso":["ChHad",1],
+channels={"M3":["M3",2],
+          "M3_Prompt":["M3_Prompt",1],
+          "M3_NonPrompt":["M3_NonPrompt",1],
+	  "noCut_ChIso_PromptPhoton":["ChHad_Prompt",1],
+          "noCut_ChIso_NonPromptPhoton":["ChHad_NonPrompt",1],
          }
 
 
@@ -22,9 +25,10 @@ process_samples ={"TTGamma":["TTGamma"],
 	          }
 process = ["TTGamma","TTbar","VGamma","Other"]	
 HistDict={}
-for c in list(channels)[:1]:
+for c in channels:
 	HistDict[c]={}
 	for p in process_samples:
+		 print "%s/phosel_%s_%s"%(process_samples[p][0],c,process_samples[p][0]) 
 		 HistDict[c][p] = _file.Get("%s/phosel_%s_%s"%(process_samples[p][0],c,process_samples[p][0])).Clone("%s_%s"%(channels[c][0],p))
 	  	 for s in process_samples[p][1:]:
 			 HistDict[c][p].Add(_file.Get("%s/phosel_%s_%s"%(s,c,s)))
@@ -53,13 +57,14 @@ def makePseudoData(HistDict,processList,processScales = None):
 
 HistDict = makePseudoData(HistDict,process)
 
-outputFile = TFile("Step1a_onlyMC.root","recreate")
+outputFile = TFile("Step3a_onlyMC.root","recreate")
 
-for c in list(channels)[:1]:
+for c in channels:
+	if c=="M3":continue
         outputFile.mkdir(c)
         for p in process+["data_obs"]:
-                outputFile.mkdir("%s/%s"%(c,p))
-                outputFile.cd("%s/%s"%(c,p))
+                outputFile.mkdir("%s/%s"%(channels[c][0],p))
+                outputFile.cd("%s/%s"%(channels[c][0],p))
                 HistDict[c][p].Write("nominal")
 
 
